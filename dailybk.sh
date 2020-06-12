367dd6258913c353041a9e33f35d24b6b5d3211f
Phil <philbone@focused.cl>
2020-06-12 14:04:47 -0400

    Simplifica mensajes de exito y error.

--- dailybk.sh
+++ dailybk.sh
@@ -10,47 +10,27 @@ mag=$'\e[1;35m'
 cyn=$'\e[1;36m'
 end=$'\e[0m'
 
+#BASE URL
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # setData 
-source data.conf
-
-# BASE URL
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" 
-
-# error messages
-ERR_MSG1="DESCARGA ABORTADA!"
-ERR_MSG2="El destino ya existe."
-ERR_MSG3="Error al ejecutar scp, ver log: $DIR/output.log"
-
-# sounds
-ERR_SOUND="$DIR/sounds/to-the-point.ogg"
-SUC_SOUND="$DIR/sounds/eventually.ogg"
-
-# success messages
-SUC_MSG1="DIRECTORIO CREADO: $dateBackup"
-SUC_MSG2="DESCARGA COMPLETADA!!"
-FIN_MSG="Hecho!"
-
-# makeDestiny()
+source $DIR/data.conf
+#makeDestiny()
 cd $destiny
 mkdir -v "$destiny/$dateBackup" &> $DIR/output.log
-if [ $? -ne 0 ] ; then        
-    notify-send -t 1000000 "$ERR_MSG1" "$ERR_MSG2" 
-    echo ${red}$ERR_MSG1, $ERR_MSG2${end}
-    paplay $ERR_SOUND
+if [ $? -ne 0 ] ; then      
+    echo "a $? el directorio origen ya existe"
 else    
-    echo ${grn}$SUC_MSG1${end}
+    echo "b $?  directorio creado: $dateBackup"
     # getCopy()
     scp -o StrictHostKeyChecking=no -i $key $user@$host:$origin "$destiny/$dateBackup" 2> $DIR/output.log    
     if [ $? -ne 0 ] ; then
-        echo ${red}$ERR_MSG3${end}
-        paplay $ERR_SOUND
-    else
-        notify-send -t 1000000 "$SUC_MSG2"
-        echo ${grn}$SUC_MSG2${end}
+        echo "c $?  no se encuentra fichero de origen"
+    else        
+        echo "d $?  descarga completada"
         # listDestiny()
-        echo "creando lista..." 
-        paplay $SUC_SOUND
-        ls --full-time -ghR &>$DIR/output.log && echo ${grn}$FIN_MSG${end}
+        echo "creando lista..."         
+        ls --full-time -ghR &>$DIR/output.log && echo "e $? terminado."
     fi
 fi
 
